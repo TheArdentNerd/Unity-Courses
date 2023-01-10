f8410908fd49a788a41d1e7e37d360a5aceb9a0e
TheArdentNerd <122030614+TheArdentNerd@users.noreply.github.com>
2023-01-10 17:42:56 -0500

    Update HelloWorld.cs

--- Unity Projects/Quiz Master/Assets/Scripts/HelloWorld.cs
+++ Unity Projects/Quiz Master/Assets/Scripts/HelloWorld.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
 
 public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.WriteLine("Hellow World");
     }
 
     // Update is called once per frame
