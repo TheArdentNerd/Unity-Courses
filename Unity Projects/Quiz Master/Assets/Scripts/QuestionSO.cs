619446b587052787bfe5abc7a864ed554a3cad68
TheArdentNerd <122030614+TheArdentNerd@users.noreply.github.com>
2023-01-16 17:00:39 -0500

    Updates

--- Unity Projects/Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Unity Projects/Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -6,13 +6,8 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
    [TextArea(2,6)]
-   // The attribute TextArea in our variable displays the minimum and maximum number 
-   //of lines shown in the inspector
    [SerializeField] string question = "Enter new question text here";
    [SerializeField]string[] answers = new string[4];
-   // So just like our question we are going to want to change htem in the inspector
-   // but we dont want to be able to change them from outside the scriptable object
-
    [SerializeField] int correctAnswerIndex;
 
    public string GetQuestion()
@@ -27,7 +22,5 @@ public class QuestionSO : ScriptableObject
    {
       return answers[index];
    }
-   // We want GetAnswer to accept a parameter of type int called index
-   // We want to return answers at the index that we specified
-   //This method is passing in an index that we want to use and returning an answer at that given index
+
 }
