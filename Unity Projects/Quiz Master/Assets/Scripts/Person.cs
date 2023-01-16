b8510fef1a7e805b21dc15c5d70958cf16473130
TheArdentNerd <122030614+TheArdentNerd@users.noreply.github.com>
2023-01-16 09:29:13 -0500

    Update Person.cs

--- Unity Projects/Quiz Master/Assets/Scripts/Person.cs
+++ Unity Projects/Quiz Master/Assets/Scripts/Person.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class Person
 {
@@ -8,5 +9,14 @@ public class Person
     public void Introduce()
     {
         Debug.Log("My name is " + FirstName + " " + LastName);
+        var firstName = "Levi";
+        String lastName = "Binder";
+
+        var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+        var names = new string[3] {"John","Jack","Mary"};
+
+        var formattedNames = string.Join(",", names);
+        Debug.Log(formattedNames);
     }
 }
